135c0e861a99737adbf8bf82a5f639b5f885fb8b
Yekta <yektakayabasi@gmail.com>
2023-09-14 12:01:28 +0300

    1409

---
+++ src/test/java/management_on_schools/step_definitions/US_24_25/US24.java
@@ -0,0 +1,22 @@
+package management_on_schools.step_definitions.US_24_25;
+
+import io.cucumber.java.en.*;
+
+public class US24 {
+    @Given("Admin Login Olur")
+    public void admin_login_olur() {
+
+    }
+    @When("Admin Teacher ile ilgili bilgileri girer")
+    public void admin_teacher_ile_ilgili_bilgileri_girer() {
+
+    }
+    @When("Admin submit tuşuna basar")
+    public void admin_submit_tuşuna_basar() {
+
+    }
+    @Then("Admin teacher listin son sayfasına giderek öğretmenin oluştuğunu kontrol eder")
+    public void admin_teacher_listin_son_sayfasına_giderek_öğretmenin_oluştuğunu_kontrol_eder() {
+
+    }
+}
